853bac481954a38664c58435ed445b7350df78e2
YONGWOOJONG <yong900305@gmail.com>
2022-10-18 14:28:18 +0900

    업로드 테스트

--- guad_frontend/src/fileUploadForm.js
+++ guad_frontend/src/fileUploadForm.js
@@ -2,10 +2,10 @@ function fileUploadForm() {
 
   return (
     <>
-    <h3>파일 업로드</h3>
-    <form id="fileUploadForm" method="post" action="/upload/fileUpload" enctype="multipart/form-data">
-      파일 : <input type="file" id="uploadFile" name="uploadFile"/>
-      <input type="submit" value="업로드"></input>
+    <h3>파일 업로드 (여러 개 파일 업로드)</h3>
+    <form id="fileUploadFormMulti" method="post" action="/upload/fileUploadMultiple" enctype="multipart/form-data">
+      파일 : <input type="file" id="uploadFileMulti" name="uploadFileMulti" multiple="multiple"/>
+      <input type="submit" value="업로드"/>
     </form>
     </>
   );
