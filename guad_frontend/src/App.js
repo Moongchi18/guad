fc367b64ae448b8180f2e35b517f275d2ef61efb
Moongchi18 <lsmunt2@gamil.com>
2022-10-18 11:42:24 +0900

    test04

--- guad_frontend/src/App.js
+++ guad_frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ChatRoom from './component/ChatRoom';
 
 function App() {
+
   return (
     <>
-    
+      <ChatRoom />
     </>
   );
 }
