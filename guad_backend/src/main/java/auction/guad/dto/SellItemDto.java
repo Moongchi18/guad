208c65860973d085b399d312368c9dec3c6f4f37
hi <hi@DESKTOP-F4BC13L>
2022-10-18 10:04:00 +0900

    Dto 추가 -우종

--- guad_backend/src/main/java/auction/guad/dto/SellItemDto.java
+++ guad_backend/src/main/java/auction/guad/dto/SellItemDto.java
@@ -1,11 +1,28 @@
 package auction.guad.dto;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class SellItemDto {
 	private String sellType;
 	private String memberEmail;
-	private int itemNum;
 	
+	private int itemNum;
+	private String itemSub;
+	private int itemPrice;
+	private String itemType;
+	private String itemDType;
+	private Date writeDate;
+	private int hitCnt;
+	private int AStartPrice;
+	private int AMaxPrice;
+	private int AMinPrice;
+	private int APeriod;
+	
+	private String soldYn;
+	private Date soldDate;
+	private String deleteYn;
+	private Date deleteDate;
 }
