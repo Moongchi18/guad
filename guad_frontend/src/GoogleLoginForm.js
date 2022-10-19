4ec2db1ab70c26363f2bc95821855bf9e722c46b
YONGWOOJONG <108851430+YONGWOOJONG@users.noreply.github.com>
2022-10-19 20:55:37 +0900

    111

--- guad_frontend/src/GoogleLoginForm.js
+++ guad_frontend/src/GoogleLoginForm.js
@@ -1,22 +1,31 @@
-import axios from "axios";
-import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GoogleLogin } from '@react-oauth/google';
+
+import {useEffect} from 'react'
 
 function GoogleLoginForm() {
+
+  function handlerCallbackResponse(response) {
+
+  }
+
+    useEffect(() => {
+      /* global google */
+      google.accounts.id.initialize({
+       client_id: "1068908873530-hp1930ja7o5k3qcree5o0v9tt21h055h.apps.googleusercontent.com",
+       callback: handlerCallbackResponse
+      });
+
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline", size: "large"}
+      );
+    }, []);
+
   return (
     <>
-      <GoogleOAuthProvider clientId="1068908873530-v0oqqriuhdbbk25d85vtau5hf39tebfo.apps.googleusercontent.com">
-        <GoogleLogin
-          buttonText="구글 로그인"        
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-      </GoogleOAuthProvider>
+    <div className="App">
+      <div id="signInDiv"></div>
+    </div>
+
     </>
   );
 }
