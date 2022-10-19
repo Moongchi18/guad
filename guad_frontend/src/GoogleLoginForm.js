6145306cb87e1f483b8d57e4a43fc4d039a3a6ac
YONGWOOJONG <108851430+YONGWOOJONG@users.noreply.github.com>
2022-10-19 22:36:37 +0900

    3

--- guad_frontend/src/GoogleLoginForm.js
+++ guad_frontend/src/GoogleLoginForm.js
@@ -1,30 +1,50 @@
-
-import {useEffect} from 'react'
+import { useEffect, useState } from 'react'
+import jwt_decode from "jwt-decode";
 
 function GoogleLoginForm() {
+  const [user, setUser] = useState({});
+
 
   function handlerCallbackResponse(response) {
+    console.log("Encoded JWT ID token " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
 
+    //로그인 하면 로그인 버튼 가리기
+    document.getElementById("signInDiv").hidden = true;
   }
 
-    useEffect(() => {
-      /* global google */
-      google.accounts.id.initialize({
-       client_id: "1068908873530-hp1930ja7o5k3qcree5o0v9tt21h055h.apps.googleusercontent.com",
-       callback: handlerCallbackResponse
-      });
+  function handleSignOut(e) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
 
-      google.accounts.id.renderButton(
-        document.getElementById("signInDiv"),
-        { theme: "outline", size: "large"}
-      );
-    }, []);
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "1068908873530-hp1930ja7o5k3qcree5o0v9tt21h055h.apps.googleusercontent.com",
+      callback: handlerCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+  }, []);
+
 
   return (
     <>
-    <div className="App">
-      <div id="signInDiv"></div>
-    </div>
+      <div className="App">
+        <div id="signInDiv"></div>
+        { }
+        <button onClick={(e) => handleSignOut(e)}>로그아웃</button>
+        {user && <div><img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>}
+      </div>
 
     </>
   );
