e5f504267622c18952e1e8830c249ca5ea3479d8
YONGWOOJONG <yong900305@gmail.com>
2022-10-20 17:09:01 +0900

    Batch 디펜던시 추가

---
+++ guad_backend/src/main/java/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class BatchApplication {
+
+    public static void main(String[] args) {
+        
+    }
+}
