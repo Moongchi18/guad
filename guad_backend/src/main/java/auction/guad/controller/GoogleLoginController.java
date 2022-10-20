5248794a1a3d2ad7574d0db8fd129fe905524b8c
YONGWOOJONG <yong900305@gmail.com>
2022-10-20 14:51:29 +0900

    컨트

--- guad_backend/src/main/java/auction/guad/controller/GoogleLoginController.java
+++ guad_backend/src/main/java/auction/guad/controller/GoogleLoginController.java
@@ -3,9 +3,13 @@ package auction.guad.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import auction.guad.dto.MemberDto;
 
 
 @RestController
@@ -13,13 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class GoogleLoginController {
 
     @RequestMapping(value = "/google/auth", method = RequestMethod.POST)
-    public ResponseEntity<String> googleAuth(@PathVariable("email") String email) throws Exception {
+    public ResponseEntity<String> googleAuth(@RequestBody MemberDto member) throws Exception {
       
-        
+        String googleId = member.getEmail();
         // 회원 조회서비스 추가 예정
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+email);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+ googleId);
        
-        if (email != null) {
+        
+        if (googleId != null) {
             return ResponseEntity.status(HttpStatus.OK).body("로그인 성공");
         } else {
             return ResponseEntity.status(300).body("로그인 실패");
