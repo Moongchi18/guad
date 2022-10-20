684638c031f30ab8ef75d52c823e0cca42f6a4ad
YONGWOOJONG <yong900305@gmail.com>
2022-10-20 17:19:22 +0900

    dd

--- guad_backend/src/main/java/batch/BatchApplication.java
+++ guad_backend/src/main/java/batch/BatchApplication.java
@@ -1,6 +1,7 @@
 package batch;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableBatchProcessing
@@ -8,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BatchApplication {
 
     public static void main(String[] args) {
-        
+        SpringApplication.run(BatchApplication.class);
     }
 }
