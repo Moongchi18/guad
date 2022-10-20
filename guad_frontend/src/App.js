25fcb3f8f58050bf64d587a546d4bb019587a864
woootae <s5s5z@naver.com>
2022-10-20 17:25:14 +0900

    태태_3

--- guad_frontend/src/App.js
+++ guad_frontend/src/App.js
@@ -11,11 +11,13 @@ function App() {
   console.log("test33333");
   return (
     <>
+      <Header />
       <Route path="/g_login" component={GoogleLoginForm} exact={true} />
       <Route path="/login" component={Login} exact={true} />
       <Route path="/upload" component={FileUploadForm} exact={true} />
       <Route path="/header" component={Header} exact={true} />
       <Route path="/footer" component={Footer} exact={true} />
+      <Footer />
     </>
   );
 }
