9f4dd8af13a8f84ca1a78e1796fb8ef1c110ba40
woootae <s5s5z@naver.com>
2022-10-20 16:15:14 +0900

    태태

--- guad_frontend/src/component/Login.js
+++ guad_frontend/src/component/Login.js
@@ -1,64 +1,50 @@
 import React from "react";
-import styled from "styled-components";
-import './Login.css';
-// import logo from './static/img/logo.jpg';
-// import './static/font/font.css';
+import "../source/Login.css";
 import axios from "axios";
 import { useState } from "react";
-
-
-const Container = styled.div`
-  
-`;
-
-const Input = styled.input`
-box-sizing: border-box;
-
-position: absolute;
-font-family: 'SBAggroB';
-font-style: normal;
-font-weight: 200;
-font-size: 20px;
-line-height: 24px;
-color: #E0E0E0;
-
-`;
-
-const Button = styled.div`
-
-    text-align: center;
- 
-    background-color: #efefef;
- `;
+import logo from "../source/img/login_logo.png";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
- const [email, setEmail] = useState('');
- const [password, setPassword] = useState('');
-
- const changeEmail = (e) => setEmail(e.target.value);
- const changePassword = (e) => setPassword(e.target.value);
- const handlerSubmit = () => {
-        axios.post('http://localhost:8080/login', {"email": email, "pass": password})
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    };
+  const changeEmail = (e) => setEmail(e.target.value);
+  const changePassword = (e) => setPassword(e.target.value);
+  const handlerSubmit = () => {
+    axios
+      .post("http://localhost:8080/login", { email: email, pass: password })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
-
-      {/* <img src={logo} id="Logo" alt="로고"/> */}
-      <span id="logintext">가입한 계정으로 로그인</span>
-      <Input id="id" name="id" placeholder="아이디" value={email} onChange={changeEmail} />
-      <Input
-        id="password"
-        name="password"
-        type="password"
-        placeholder="비밀번호"
-        value={password} onChange={changePassword}
-      />
-      <Button id="login" onClick={handlerSubmit}>로그인</Button>
-      <Button id="join">회원가입</Button>
+      <div className="All_box">
+        <div className="login_box">
+          <img src={logo} className="Logo" alt="로고" />
+          <span className="logintext">가입한 계정으로 로그인</span>
+          <input
+            className="in_box"
+            placeholder="아이디"
+            value={email}
+            onChange={changeEmail}
+          />
+          <input
+            className="in_box"
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={changePassword}
+          />
+          <span className="check_b">아이디저장</span>
+          <button className="login" onClick={handlerSubmit} type="button">
+            로그인
+          </button>
+          <button className="join" type="button">
+            회원가입
+          </button>
+        </div>
+      </div>
     </>
   );
 }
