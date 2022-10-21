7e67f1575d93de7951b515851bb2e4624888aa5d
Moongchi18 <lsmunt2@gamil.com>
2022-10-21 12:40:25 +0900

    temp

--- guad_frontend/src/component/Auction.js
+++ guad_frontend/src/component/Auction.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import {over} from 'stompjs';
+import { over } from 'stompjs';
 import SockJS from 'sockjs-client';
 
-var stompClient =null;
+var stompClient = null;
 const Auction = () => {
     const [bidPrice, setBidPrice] = useState();
     const [priceList, setPriceList] = useState([]);
@@ -12,7 +12,7 @@ const Auction = () => {
         message: '',
         bidPrice: '',
         itemNum: 0
-      });
+    });
     useEffect(() => {
         setBidPrice(userData.bidPrice)
     }, [userData]);
@@ -22,63 +22,63 @@ const Auction = () => {
 
     }, [])
 
-    const connect =()=>{
+    const connect = () => {
         let Sock = new SockJS('http://192.168.0.35:8080/ws');
         stompClient = over(Sock);
-        stompClient.connect({},onConnected, onError);
+        stompClient.connect({}, onConnected, onError);
     }
 
     const onConnected = () => {
         stompClient.subscribe('/sub/public', onMessageReceived);
     }
-    const onMessageReceived = (payload)=>{
+    const onMessageReceived = (payload) => {
         var payloadData = JSON.parse(payload.body);
         console.log(payloadData)
-        setUserData({...userData, "bidPrice":payloadData.bidPrice})
+        setUserData({ ...userData, "bidPrice": payloadData.bidPrice })
         const newPriceList = priceList
         newPriceList.push(payloadData.bidPrice)
         setPriceList(newPriceList)
     }
     const onError = (err) => {
         console.log(err);
-        
+
     }
 
     const handleBidPrice = (e) => setBidPrice(e.target.value)
 
-    const sendValue=()=>{
+    const sendValue = () => {
         console.log("send : " + userData.bidPrice)
-            if (stompClient) {
-              var chatMessage = {
+        if (stompClient) {
+            var chatMessage = {
                 senderName: userData.username,
                 message: userData.message,
                 bidPrice: bidPrice
-              };
-              console.log(chatMessage);
-              stompClient.send("/pub/message", {}, JSON.stringify(chatMessage));
-              setUserData({...userData,"message": ""});
-            }
+            };
+            console.log(chatMessage);
+            stompClient.send("/pub/message", {}, JSON.stringify(chatMessage));
+            setUserData({ ...userData, "message": "" });
+        }
     }
     console.log(priceList)
     return (
-    <div className="container">
-        <div className="chat-box">
-            <div className="chat-content">
-                <ul className="chat-messages">
-                    {priceList.map((p, i) => (
-                        <li key={i}>{p}</li>
-                    ))}
-                </ul>
-                <div className="send-message">
-                    <input type="text" className="input-message" placeholder="enter the price" value={bidPrice} onChange={handleBidPrice} /> 
-                    <button type="button" className="send-button" onClick={sendValue}>send</button>
+        <div className="container">
+            <div className="chat-box">
+                <div className="chat-content">
+                    <ul className="chat-messages">
+                        {priceList.map((p, i) => (
+                            <li key={i}>{p}</li>
+                        ))}
+                    </ul>
+                    <div className="send-message">
+                        <input type="text" className="input-message" placeholder="enter the price" value={bidPrice} onChange={handleBidPrice} />
+                        <button type="button" className="send-button" onClick={sendValue}>send</button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <h1 style={{textAlign: "center"}}>현재 입찰 가격</h1>
-        <h1 style={{textAlign: "center"}}>{userData.bidPrice}</h1>
-    </div>
+            <h1 style={{ textAlign: "center" }}>현재 입찰 가격</h1>
+            <h1 style={{ textAlign: "center" }}>{userData.bidPrice}</h1>
+        </div>
     )
 }
 
