80f4a7548fc0c31f6711eac5571720dc0ba1132f
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:37:19 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/mapper/ImgMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/ImgMapper.java
@@ -1,8 +1,19 @@
 package auction.guad.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import auction.guad.dto.ImgDto;
 
 @Mapper
 public interface ImgMapper {
-
+	
+	int insertSellImg(ImgDto imgDto) throws Exception;
+	int insertReviewImg(ImgDto imgDto) throws Exception;
+	ImgDto selectImgByItemImgNum(int itemImgNum) throws Exception;
+	List<ImgDto> allImgByItemNum(int itemNum) throws Exception;
+	int deleteImg(int itemImgNum) throws Exception;
+	
+	
 }
