4078e62033cc4e9f99383741e9dfee8b6319e9d3
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:32:03 +0900

    Update SellItemMapper.java

--- guad_backend/src/main/java/auction/guad/mapper/SellItemMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/SellItemMapper.java
@@ -1,8 +1,19 @@
 package auction.guad.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import auction.guad.dto.SellItemDto;
 
 @Mapper
 public interface SellItemMapper {
 
+	public List<SellItemDto> sellItemList() throws Exception;
+	public int insertSellItem(SellItemDto sellItemDto) throws Exception;
+	public SellItemDto selectSellItemDetail(int itemNum) throws Exception;
+	public void updateSellItem(SellItemDto sellItemDto) throws Exception;
+	public void deleteSellItem(int itemNum) throws Exception;
+
+	
 }
