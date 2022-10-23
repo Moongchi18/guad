4d879def70af888d78245b5191c2a7f25e41be62
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 16:01:00 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/mapper/CommentsMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/CommentsMapper.java
@@ -9,9 +9,10 @@ import auction.guad.dto.CommentsDto;
 @Mapper
 public interface CommentsMapper {
 
-	public List<CommentsDto> commentsList() throws Exception;
-	public int insertComment(CommentsDto commentsDto) throws Exception;
-	public CommentsDto selectCommentDetail(int commentNum) throws Exception;
-	public void updateComment(CommentsDto commentsDto) throws Exception;
-	public void deleteComment(int commentNum) throws Exception;
+List<CommentsDto> commentsList() throws Exception;
+int insertComment(CommentsDto commentsDto) throws Exception;
+CommentsDto selectCommentDetail(int commentNum) throws Exception;
+void updateComment(CommentsDto commentsDto) throws Exception;
+void deleteComment(int commentNum) throws Exception;
+	
 }
