dd477f5fcabfd4378f78f6d3c7c537f1f7e8e76b
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:53:47 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/service/CommentsServiceImpl.java
+++ guad_backend/src/main/java/auction/guad/service/CommentsServiceImpl.java
@@ -1,5 +1,41 @@
 package auction.guad.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import auction.guad.dto.CommentsDto;
+import auction.guad.mapper.CommentsMapper;
+
 public class CommentsServiceImpl implements CommentsService{
 
+	
+	@Autowired
+	private CommentsMapper commentsMapper;
+	
+	public List<CommentsDto> commentsList() throws Exception {
+		return commentsMapper.commentsList();		
+	}
+	
+	public int insertComment(CommentsDto commentsDto) throws Exception {
+		return commentsMapper.insertComment(commentsDto);
+		
+	}
+	
+	
+	public CommentsDto selectCommentDetail(int commentNum) throws Exception {
+		return commentsMapper.selectCommentDetail(commentNum);
+		
+	}
+	
+	public void updateComment(CommentsDto commentsDto) throws Exception {
+		    commentsMapper.updateComment(commentsDto);
+		
+	}
+	
+	public void deleteComment(int commentNum) throws Exception {
+			commentsMapper.deleteComment(commentNum);
+	}
+
+	
 }
