8282b910c94af056096d64f5ca2bdaf570e6f2e7
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:36:22 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/controller/ImgController.java
+++ guad_backend/src/main/java/auction/guad/controller/ImgController.java
@@ -1,12 +1,46 @@
 package auction.guad.controller;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import auction.guad.dto.ImgDto;
+import auction.guad.service.ImgService;
 import auction.guad.service.ImgServiceImpl;
 
 @RestController
 public class ImgController {
 
+	@Autowired
+	private ImgService imgService;
+	
+	
+	@RequestMapping(value="/img", method=RequestMethod.GET)
+	public List<ImgDto> allImgByItemNum(int itemNum) throws Exception {
+		return imgService.allImgByItemNum(itemNum);
+	}
+	
+	@RequestMapping(value="/img/insert/S", method=RequestMethod.POST)
+	public void insertSellImg(@RequestBody ImgDto imgDto) throws Exception {
+		imgService.insertSellImg(imgDto);
+	}
+	
+	@RequestMapping(value="/img/insert/R", method=RequestMethod.POST)
+	public void insertReviewImg(@RequestBody ImgDto imgDto) throws Exception {
+		imgService.insertReviewImg(imgDto);
+	}
+			
+	@RequestMapping(value="/img/{itemImgNum}", method=RequestMethod.PATCH)
+	public void deleteImg(@PathVariable("itemImgNum") int itemImgNum, @RequestBody ImgDto imgDto) throws Exception {
+		imgDto.setItemImgNum(itemImgNum);
+		imgService.deleteImg(itemImgNum);
+	}
+
+	
 }
