741c2ccf99dae6c209d8032f164ce9e42bee5065
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:58:21 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/mapper/ReviewMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/ReviewMapper.java
@@ -1,8 +1,17 @@
 package auction.guad.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import auction.guad.dto.ReviewDto;
 
 @Mapper
 public interface ReviewMapper {
 
+	public List<ReviewDto> reviewList() throws Exception;
+	public int insertReview(ReviewDto reviewDto) throws Exception;
+	public ReviewDto selectReviewDetail(int reviewNum) throws Exception;
+	public void updateReview(ReviewDto reviewDto) throws Exception;
+	public void deleteReview(int reviewNum) throws Exception;
 }
