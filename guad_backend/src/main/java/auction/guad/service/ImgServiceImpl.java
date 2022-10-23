624e42af9c32dc6ee567680d1b5a2cd38c1ca6c1
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:36:58 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/service/ImgServiceImpl.java
+++ guad_backend/src/main/java/auction/guad/service/ImgServiceImpl.java
@@ -1,5 +1,44 @@
 package auction.guad.service;
 
-public class ImgServiceImpl implements ImgService{
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import auction.guad.dto.ImgDto;
+import auction.guad.mapper.ImgMapper;
+
+@Service
+public class ImgServiceImpl implements ImgService {
+
+	@Autowired
+	private ImgMapper imgMapper;
+	
+	@Override
+	public int insertSellImg(ImgDto imgDto) throws Exception {
+		return imgMapper.insertSellImg(imgDto);
+	}
+		
+	@Override
+	public int insertReviewImg(ImgDto imgDto) throws Exception {
+		return imgMapper.insertReviewImg(imgDto);
+	}
+
+	@Override
+	public ImgDto selectImgByItemImgNum(int itemImgNum) throws Exception {
+		return imgMapper.selectImgByItemImgNum(itemImgNum);
+	}
+	
+	@Override
+	public List<ImgDto> allImgByItemNum(int itemNum) throws Exception {
+		 return imgMapper.allImgByItemNum(itemNum);
+	}
+
+
+	@Override
+	public int deleteImg(int itemImgNum) throws Exception {
+			return imgMapper.deleteImg(itemImgNum);
+	}
+	
 }
+
