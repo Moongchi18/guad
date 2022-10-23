26ba551b18a158faa622caf4b6ce0ae3f26b7b1e
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:53:29 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/service/CommentsService.java
+++ guad_backend/src/main/java/auction/guad/service/CommentsService.java
@@ -1,5 +1,14 @@
 package auction.guad.service;
 
+import java.util.List;
+
+import auction.guad.dto.CommentsDto;
+
 public interface CommentsService {
 
+	public List<CommentsDto> commentsList() throws Exception;
+	public int insertComment(CommentsDto commentsDto) throws Exception;
+	public CommentsDto selectCommentDetail(int commentNum) throws Exception;
+	public void updateComment(CommentsDto commentsDto) throws Exception;
+	public void deleteComment(int commentNum) throws Exception;
 }
