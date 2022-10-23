6f4383db3e27c5b48e1ecbb53a056909f3dcea1d
smilews <115124176+smilews@users.noreply.github.com>
2022-10-23 15:33:46 +0900

    기본 작성

--- guad_backend/src/main/java/auction/guad/service/SellItemServiceImpl.java
+++ guad_backend/src/main/java/auction/guad/service/SellItemServiceImpl.java
@@ -1,5 +1,41 @@
 package auction.guad.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import auction.guad.dto.SellItemDto;
+import auction.guad.mapper.SellItemMapper;
+
+@Service
 public class SellItemServiceImpl implements SellItemService{
+	
+	@Autowired
+	private SellItemMapper sellItemMapper;
+	
+	public List<SellItemDto> sellItemList() throws Exception {
+		return sellItemMapper.sellItemList();		
+	}
+	
+	public int insertSellItem(SellItemDto sellItemDto) throws Exception {
+		return sellItemMapper.insertSellItem(sellItemDto);
+		
+	}
+	
+	
+	public SellItemDto selectSellItemDetail(int itemNum) throws Exception {
+		return sellItemMapper.selectSellItemDetail(itemNum);
+		
+	}
+	
+	public void updateSellItem(SellItemDto sellItemDto) throws Exception {
+		    sellItemMapper.updateSellItem(sellItemDto);
+		
+	}
+	
+	public void deleteSellItem(int itemNum) throws Exception {
+			sellItemMapper.deleteSellItem(itemNum);
+	}
 
 }
