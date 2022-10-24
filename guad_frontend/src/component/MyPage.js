07113f01eb09176ecfff53441789c34b09ebb9eb
YONGWOOJONG <yong900305@gmail.com>
2022-10-24 14:35:44 +0900

    이미지 추가

--- guad_frontend/src/component/MyPage.js
+++ guad_frontend/src/component/MyPage.js
@@ -1,34 +1,45 @@
 import "../source/MyPage.css";
+import logo from "../source/img/mypage.png";
 
-function MyPage() {
+function Mypage() {
   return (
     <>
-      <div>
-        <div className="All_Mbox">
-            <h1 className="page_name">
-                   마이페이지
-            </h1>
-            <div>
-                <div className="Mbox">
-                    사진, 글, 버튼
-                </div>
+      <div className="All_Mbox">
+        <div className="All_Mbox2">
+          <h1 className="page_name">마이페이지</h1>
+          <div>
+            <div className="Mbox">
+              <div className="logo_box">
+                <img src={logo}></img>
+              </div>
+              <div className="mileage_box">
+                <h3><strong>시흥 기린</strong>님 환영합니다!</h3>
+                <h3> 현재마일리지 <strong>1,000,000</strong>원</h3>
+              </div>
+              <div className="Mbox_button">
+                <button className="member">회원정보</button>
+                <button>마일리지</button>
+              </div>
             </div>
-            <div className="category">
-                <h3>등록 상품 내역</h3>
+          </div>
+          <div className="category">
+            <h3>등록 상품 내역</h3>
+          </div>
+          <div>
+            <div className="category_list">
+                <img></img>
             </div>
-            <div>
-                <div className="category_list">상품이미지</div>
-            </div>
-            <div className="category">
-                <h3>상품 구매 내역</h3>
-            </div>
-            <div>
-                <div className="category_list">상품이미지</div>
-            </div>
+          </div>
+          <div className="category">
+            <h3>상품 구매 내역</h3>
+          </div>
+          <div>
+            <div className="category_list">상품이미지</div>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default MyPage;
+export default Mypage;
