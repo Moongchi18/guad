0e8c7249cc6947185207e4a602cc8237b0b90803
smilews <115124176+smilews@users.noreply.github.com>
2022-10-24 14:36:12 +0900

    임시

--- guad_backend/src/main/java/auction/guad/mapper/MileageMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/MileageMapper.java
@@ -1,8 +1,16 @@
 package auction.guad.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import auction.guad.dto.MileageDto;
 
 @Mapper
 public interface MileageMapper {
-
+ 
+	
+	int chargeMileage(MileageDto mileageDto) throws Exception;
+	MileageDto inquireMileageByEmail(String memberEmail) throws Exception;
+	
 }
