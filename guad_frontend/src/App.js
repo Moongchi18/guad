ce30b912186531dfe5644b8745f5d7d564406930
Moongchi18 <lsmunt2@gamil.com>
2022-10-24 18:07:50 +0900

    security ~ manager role

--- guad_frontend/src/App.js
+++ guad_frontend/src/App.js
@@ -8,22 +8,11 @@ import AuctionTest from "./auction/AuctionTest";
 import Auction from "./component/Auction";
 import Footer from "./component/Footer";
 import Main from "./component/Main";
-
-import Mypage from "./component/Mypage";
-import MypageInfo from "./component/MypageInfo";
-
 import Join from "./component/Join";
-import JoinG from "./component/JoinG";
-import SellList from "./component/SellList";
-import axios from "axios";
-import SellItem from "./component/SellItem";
+import Join_g from "./component/Join_g";
+import MyPage from "./component/MyPage";
 
 function App() {
-  axios.interceptors.request.use(function (config) {
-    const token = sessionStorage.getItem("token");
-    config.headers.Authorization = token ? `Bearer ${token}` : "";
-    return config;
-  });
   return (
     <>
       <Header />
@@ -37,17 +26,11 @@ function App() {
         exact={true}
       />
       <Route path="/auction" component={Auction} exact={true} />
-      <Route path="/mypage" component={Mypage} exact={true} />
+      <Route path="/mypage" component={MyPage} exact={true} />
       <Route path="/footer" component={Footer} exact={true} />
       <Route path="/" component={Main} exact={true} />
-
-      <Route path="/mypage_info" component={MypageInfo} exact={true} />
-
       <Route path="/join" component={Join} exact={true} />
-      <Route path="/join_g" component={JoinG} exact={true} />
-      <Route path="/sell_list" component={SellList} exact={true} />
-      <Route path="/sell_item" component={SellItem} exact={true} />
-
+      <Route path="/join_g" component={Join_g} exact={true} />
       <Footer />
     </>
   );
