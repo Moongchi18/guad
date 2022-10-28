52d4d199d2621b1e8fb53317de50b2f050a4fb7c
woootae <s5s5z@naver.com>
2022-10-28 14:56:00 +0900

    태태

--- guad_frontend/src/component/Moodal/BuyReview.js
+++ guad_frontend/src/component/Moodal/BuyReview.js
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import style from "../../source/BuyReview.module.css";
 
 function BuyReview({ modalOpen, closeModal }) {
+  const [rating, setRating] = useState("");
+  const ratingClick = (e) => {
+    const star = e.target.name;
+    if (star == "1") {
+      setRating("1");
+    } else if (star == "2") {
+      setRating("2");
+    } else if (star == "3") {
+      setRating("3");
+    } else if (star == "4") {
+      setRating("4");
+    } else {
+      setRating("5");
+    }
+  };
+
   return (
     <>
       <div className={style.modal} ref={modalOpen}>
@@ -14,11 +31,63 @@ function BuyReview({ modalOpen, closeModal }) {
           </div>
           <div className={style.modalbody}>
             <div className={style.rating}>
-              <img src={require("../../source/img/rating2.png")} alt="별점" />
-              <img src={require("../../source/img/rating2.png")} alt="별점" />
-              <img src={require("../../source/img/rating2.png")} alt="별점" />
-              <img src={require("../../source/img/rating2.png")} alt="별점" />
-              <img src={require("../../source/img/rating2.png")} alt="별점" />
+              <img
+                src={
+                  rating == "1" ||
+                  rating == "2" ||
+                  rating == "3" ||
+                  rating == "4" ||
+                  rating == "5"
+                    ? require("../../source/img/rating1.png")
+                    : require("../../source/img/rating2.png")
+                }
+                alt="별점"
+                name="1"
+                onClick={ratingClick}
+              />
+              <img
+                src={
+                  rating == "2" ||
+                  rating == "3" ||
+                  rating == "4" ||
+                  rating == "5"
+                    ? require("../../source/img/rating1.png")
+                    : require("../../source/img/rating2.png")
+                }
+                alt="별점"
+                name="2"
+                onClick={ratingClick}
+              />
+              <img
+                src={
+                  rating == "3" || rating == "4" || rating == "5"
+                    ? require("../../source/img/rating1.png")
+                    : require("../../source/img/rating2.png")
+                }
+                alt="별점"
+                name="3"
+                onClick={ratingClick}
+              />
+              <img
+                src={
+                  rating == "4" || rating == "5"
+                    ? require("../../source/img/rating1.png")
+                    : require("../../source/img/rating2.png")
+                }
+                alt="별점"
+                name="4"
+                onClick={ratingClick}
+              />
+              <img
+                src={
+                  rating == "5"
+                    ? require("../../source/img/rating1.png")
+                    : require("../../source/img/rating2.png")
+                }
+                alt="별점"
+                name="5"
+                onClick={ratingClick}
+              />
             </div>
             <p className={style.ask}>얼마나 만족스러우셨나요?</p>
             <p className={style.seller}>
