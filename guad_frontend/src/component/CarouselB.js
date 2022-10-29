57b2ee242f4194d5c4062aa91c429a1af4df215b
woootae <s5s5z@naver.com>
2022-10-29 20:32:12 +0900

    태태

--- guad_frontend/src/component/CarouselB.js
+++ guad_frontend/src/component/CarouselB.js
@@ -1,10 +1,14 @@
+import { useEffect, useRef } from "react";
 import style from "../source/Carousel.module.css";
 
 function CarouselB() {
+  
+
+  useEffect(() => {}, []);
   return (
     <>
-      <h2>여긴 메인이벤트영역</h2>
-    </>
+    
+      </>
   );
 }
 export default CarouselB;
