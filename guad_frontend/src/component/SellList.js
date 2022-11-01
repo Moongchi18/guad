5503d5446b37c077d59392552572f31ed2e00e2c
woootae <s5s5z@naver.com>
2022-11-01 16:38:01 +0900

    태태

--- guad_frontend/src/component/SellList.js
+++ guad_frontend/src/component/SellList.js
@@ -7,59 +7,62 @@ function Sell_List() {
   const [data, setData] = useState([]);
   const [itemTypeList, setItemTypeList] = useState([]);
   const [sellItemDto, setSellItemDto] = useState({
-    itemNum: '',
-    sellType: '',
-    memberEmail: '',
-    itemSub: '',
-    itemContents: '',
-    itemPrice: '',
-    itemType: '',
-    itemDType: '',
-    writeDate: '',
-    hitCnt: '',
-    auctionStartPrice: '',
-    auctionMaxPrice: '',
-    auctionMinPrice: '',
-    auctionPeriod: '',
-    auctionRandomMethod: '',
-    auctionDiscountPerHour: '',
-    soldYn: '',
-    deleteYn: ''
-  })
+    itemNum: "",
+    sellType: "",
+    memberEmail: "",
+    itemSub: "",
+    itemContents: "",
+    itemPrice: "",
+    itemType: "",
+    itemDType: "",
+    writeDate: "",
+    hitCnt: "",
+    auctionStartPrice: "",
+    auctionMaxPrice: "",
+    auctionMinPrice: "",
+    auctionPeriod: "",
+    auctionRandomMethod: "",
+    auctionDiscountPerHour: "",
+    soldYn: "",
+    deleteYn: "",
+  });
 
   const checkSellType = (e) => {
     const gory = e.target.name;
     if (gory === "all") {
-      setSellItemDto({ ...sellItemDto, sellType: '' })
+      setSellItemDto({ ...sellItemDto, sellType: "" });
     } else if (gory === "up") {
-      setSellItemDto({ ...sellItemDto, sellType: 'u' })
+      setSellItemDto({ ...sellItemDto, sellType: "u" });
     } else if (gory === "down") {
-      setSellItemDto({ ...sellItemDto, sellType: 'd' })
-    } else if (gory === 'normal') {
-      setSellItemDto({ ...sellItemDto, sellType: 'n' })
+      setSellItemDto({ ...sellItemDto, sellType: "d" });
+    } else if (gory === "normal") {
+      setSellItemDto({ ...sellItemDto, sellType: "n" });
     }
   };
 
   useEffect(() => {
-    axios.get("http://localhost:8080/category/distinct")
-      .then(response => {
+    axios
+      .get("http://localhost:8080/category/distinct")
+      .then((response) => {
         const list = [];
-        response.data.map(d => list.push(d.itemType))
+        response.data.map((d) => list.push(d.itemType));
         setItemTypeList(list);
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error));
 
-    axios.get("http://localhost:8080/sellitem")
-      .then(response => {
-        setData(response.data.itemList)
+    axios
+      .get("http://localhost:8080/sellitem")
+      .then((response) => {
+        setData(response.data.itemList);
       })
-      .catch(error => console.log(error))
-  }, [])
+      .catch((error) => console.log(error));
+  }, []);
 
   // const handlerItemType = (e) => setItemType(e.target.value);
-  const handlerItemType = (e) => setSellItemDto({ ...sellItemDto, "sellType": e.target.value });
+  const handlerItemType = (e) =>
+    setSellItemDto({ ...sellItemDto, sellType: e.target.value });
 
-  console.log(data)
+  console.log(data);
   return (
     <>
       <div className={style.sell_all}>
@@ -69,9 +72,12 @@ function Sell_List() {
           </h2>
           <select value={sellItemDto.itemType} onChange={handlerItemType}>
             <option value="전체">전체</option>
-            {itemTypeList && itemTypeList.map((type, index) =>
-              <option key={index} value={type}>{type}</option>
-            )}
+            {itemTypeList &&
+              itemTypeList.map((type, index) => (
+                <option key={index} value={type}>
+                  {type}
+                </option>
+              ))}
           </select>
           <ul>
             <li>
@@ -80,7 +86,9 @@ function Sell_List() {
                 name="all"
                 onClick={checkSellType}
                 className={
-                  sellItemDto.sellType === "" ? `${style.cate_true}` : `${style.cate_false}`
+                  sellItemDto.sellType === ""
+                    ? `${style.cate_true}`
+                    : `${style.cate_false}`
                 }
               >
                 전 품목
@@ -92,7 +100,9 @@ function Sell_List() {
                 name="up"
                 onClick={checkSellType}
                 className={
-                  sellItemDto.sellType === "u" ? `${style.cate_true}` : `${style.cate_false}`
+                  sellItemDto.sellType === "u"
+                    ? `${style.cate_true}`
+                    : `${style.cate_false}`
                 }
               >
                 오름경매
@@ -104,7 +114,9 @@ function Sell_List() {
                 name="down"
                 onClick={checkSellType}
                 className={
-                  sellItemDto.sellType === "d" ? `${style.cate_true}` : `${style.cate_false}`
+                  sellItemDto.sellType === "d"
+                    ? `${style.cate_true}`
+                    : `${style.cate_false}`
                 }
               >
                 내림경매
@@ -116,7 +128,9 @@ function Sell_List() {
                 name="normal"
                 onClick={checkSellType}
                 className={
-                  sellItemDto.sellType === "n" ? `${style.cate_true}` : `${style.cate_false}`
+                  sellItemDto.sellType === "n"
+                    ? `${style.cate_true}`
+                    : `${style.cate_false}`
                 }
               >
                 일반판매
@@ -126,55 +140,30 @@ function Sell_List() {
         </div>
         <div className={style.sell_bot}>
           <ul>
-            <li className={style.item_info}>
-              {sellItemDto.sellType === '' && sellItemDto.itemType === '' &&
-                data.map((item, index) => (
+            {sellItemDto.sellType === "" &&
+              sellItemDto.itemType === "" &&
+              data.map((item, index) => (
+                <li className={style.item_info}>
                   <Link to={`/sell_item/${item.itemNum}`} key={index}>
                     <div className={style.item_bb}>
-                      <img src={require("../source/img/item01.png")} alt="제품1" />
+                      <img
+                        src={require("../source/img/item01.png")}
+                        alt="제품1"
+                      />
                     </div>
                     <span className={style.tex1}>{item.itemType}</span>
-                    <span className={style.tex2}>
-                      {item.itemSub}
-                    </span>
+                    <span className={style.tex2}>{item.itemSub}</span>
                     <span className={style.tex3}>
-                      {item.sellType === 'n' ? '판매가격':'경매시작가'}<strong>{item.sellType === 'n' ? item.itemPrice:item.auctionStartPrice}</strong>
+                      {item.sellType === "n" ? "판매가격" : "경매시작가"}
+                      <strong>
+                        {item.sellType === "n"
+                          ? item.itemPrice
+                          : item.auctionStartPrice}
+                      </strong>
                     </span>
                   </Link>
-                ))
-              }
-              <Link to="/sell_item/">
-                <div className={style.item_bb}>
-                  <img src={require("../source/img/item01.png")} alt="제품1" />
-                </div>
-                <span className={style.tex1}>의류/가방</span>
-                <span className={style.tex2}>
-                  디올 오피디아 스몰 패턴 도트백
-                </span>
-                <span className={style.tex3}>
-                  경매시작가<strong>450,000</strong>
-                </span>
-              </Link>
-            </li>
-            <li className={style.item_info}>
-              <Link to="/sell_item">
-                <div className={style.item_bb}>
-                  <img src={require("../source/img/item01.png")} alt="제품1" />
-                  <img
-                    src={require("../source/img/del1.png")}
-                    alt="내림경매"
-                    className={style.del_icon}
-                  />
-                </div>
-                <span className={style.tex1}>의류/가방</span>
-                <span className={style.tex2}>
-                  디올 오피디아 스몰 패턴 도트백
-                </span>
-                <span className={style.tex3}>
-                  경매시작가<strong>450,000</strong>
-                </span>
-              </Link>
-            </li>
+                </li>
+              ))}
           </ul>
           <span className={style.count_p}>
             <ul>
