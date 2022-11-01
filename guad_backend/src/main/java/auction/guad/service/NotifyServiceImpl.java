347ba385569ff29a5eb03aedb67fe5583bcdf5b7
smilews <smilews91@gmail.com>
2022-11-01 14:55:56 +0900

    신고기능 추가

--- guad_backend/src/main/java/auction/guad/service/NotifyServiceImpl.java
+++ guad_backend/src/main/java/auction/guad/service/NotifyServiceImpl.java
@@ -1,8 +1,11 @@
 package auction.guad.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import auction.guad.dto.MemberDto;
 import auction.guad.dto.NotifyDto;
 import auction.guad.mapper.NotifyMapper;
 
@@ -12,9 +15,25 @@ public class NotifyServiceImpl implements NotifyService{
 	@Autowired
 	NotifyMapper notifyMapper;
 	
+	@Override
 	public int insertNotify(NotifyDto notify) throws Exception {
 		
 		return notifyMapper.insertNotify(notify);
 		
 	}
+	
+	@Override
+	public ArrayList<NotifyDto> notifyList()throws Exception {
+		
+		return notifyMapper.notifyList();
+		
+	}
+
+	@Override
+	public NotifyDto notifyDetail(String notifyNum) throws Exception {
+		
+		return notifyMapper.notifyDetail(notifyNum);
+		
+	}
+	
 }
