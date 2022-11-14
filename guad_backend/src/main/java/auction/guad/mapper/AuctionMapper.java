73fdf8a718a246a4164c1308b569c12055a968bb
smilews <smilews91@gmail.com>
2022-11-14 11:21:34 +0900

    내림랜덤 작성

--- guad_backend/src/main/java/auction/guad/mapper/AuctionMapper.java
+++ guad_backend/src/main/java/auction/guad/mapper/AuctionMapper.java
@@ -1,8 +1,11 @@
 package auction.guad.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import auction.guad.controller.model.Auction;
+import auction.guad.dto.AuctionDownDto;
 
 
 @Mapper
@@ -10,5 +13,7 @@ public interface AuctionMapper {
 
 	int tryAuction(Auction auc) throws Exception;
 	void cancelAuction(Auction auc) throws Exception;
-	
+	int naelimRandomCount(int itemNum) throws Exception;
+	void naelimRandomPerDiscountInsert(int perDiscount, int itemNum) throws Exception;
+	List<AuctionDownDto> naelimRandomPerDiscountAll (int itemNum) throws Exception;
 }
