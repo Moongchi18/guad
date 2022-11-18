bbb6c786ccce6b67a9b8f9a8517696c7a4dcf796
woootae <s5s5z@naver.com>
2022-11-18 17:48:25 +0900

    태태

--- guad_frontend/src/component/CarouselB.js
+++ guad_frontend/src/component/CarouselB.js
@@ -6,11 +6,13 @@ function CarouselB() {
   return (
     <>
       <div className={style.all_cont}>
+        <div className={style.in_cont}>
         <h3>GREAT</h3>
         <h3>SECOND</h3>
         <h3>HAND</h3>
         <h3>AUCTION</h3>
         <h3>OLENAELIM</h3>
+        </div>
       </div>
     </>
   );
