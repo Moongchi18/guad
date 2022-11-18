4ef7bb8980796483f166081173fb39752d09e3de
moongchi18 <103802126+moongchi18@users.noreply.github.com>
2022-11-18 15:36:50 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,22 @@
-echo docker-compose up
+# echo docker-compose up
 
 
 
 
-# PROJECT_ROOT="/home/ubuntu/app"
-# JAR_FILE="$PROJECT_ROOT/springboot-app.jar"
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/springboot-app.jar"
 
-# APP_LOG="$PROJECT_ROOT/application.log"
-# ERROR_LOG="$PROJECT_ROOT/error.log"
-# DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-# TIME_NOW=$(date +%c)
+TIME_NOW=$(date +%c)
 
-# echo "$TIME_NOW : copy $JAR_FILE" >> $DEPLOY_LOG
-# cp $PROJECT_ROOT/Sample/target/*.jar $JAR_FILE
+echo "$TIME_NOW : copy $JAR_FILE" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/Sample/target/*.jar $JAR_FILE
 
-# echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
-# nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "$TIME_NOW : run $JAR_FILE" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-# CURRENT_PID=$(pgrep -f $JAR_FILE)
-# echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW : process id is $CURRENT_PID" >> $DEPLOY_LOG
