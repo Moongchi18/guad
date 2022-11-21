7a0efa00705d370a12cf5d5a9aabb876a340eee7
smilews <smilews91@gmail.com>
2022-11-21 16:55:39 +0900

    스케쥴러

--- guad_backend/src/main/java/auction/guad/service/SchedulerServiceImpl.java
+++ guad_backend/src/main/java/auction/guad/service/SchedulerServiceImpl.java
@@ -1,5 +1,29 @@
 package auction.guad.service;
 
-public class SchedulerServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import auction.guad.dto.SellItemDto;
+import auction.guad.mapper.SchedulerMapper;
+
+public class SchedulerServiceImpl {
+	
+	@Autowired
+	SchedulerMapper schedulerMapper;
+
+	public List<SellItemDto> auctionPeriodCheck() throws Exception {
+		return schedulerMapper.auctionPeriodCheck();
+	}
+	
+	public void auctionSellitemUpdate(SellItemDto sellItem) throws Exception{
+		schedulerMapper.auctionSellitemUpdate(sellItem);
+	}
+	
+	public void auctionDelete(SellItemDto sellItem) throws Exception {
+		schedulerMapper.auctionDelete(sellItem);
+	}
+	public int auctionResultInsert(SellItemDto sellItem) throws Exception {
+		return schedulerMapper.auctionResultInsert(sellItem);
+	}
 }
