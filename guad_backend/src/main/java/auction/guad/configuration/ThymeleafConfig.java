dcb5b132db172deb6fb0baf3fd5e444512670a74
smilews <smilews91@gmail.com>
2022-11-23 15:14:14 +0900

    용용

--- guad_backend/src/main/java/auction/guad/configuration/ThymeleafConfig.java
+++ guad_backend/src/main/java/auction/guad/configuration/ThymeleafConfig.java
@@ -22,7 +22,7 @@ public class ThymeleafConfig {
     public SpringResourceTemplateResolver springResourceTemplateResolver() {
         SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
         springResourceTemplateResolver.setOrder(1);
-        springResourceTemplateResolver.setPrefix("classpath:templates/");
+        springResourceTemplateResolver.setPrefix("classpath:html/");
         springResourceTemplateResolver.setSuffix(".html");
         springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
         springResourceTemplateResolver.setCharacterEncoding("UTF-8");
