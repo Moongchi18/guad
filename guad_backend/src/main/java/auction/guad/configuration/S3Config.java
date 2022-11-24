63dd49b4ffb7f9f4ef1bde0f0501b071514bb9e1
Moongchi18 <lsmunt2@gamil.com>
2022-11-24 15:38:29 +0900

    tt

--- guad_backend/src/main/java/auction/guad/configuration/S3Config.java
+++ guad_backend/src/main/java/auction/guad/configuration/S3Config.java
@@ -13,22 +13,22 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 @Configuration
 public class S3Config {
 
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-	
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
-	
-	@Value("${cloud.aws.region.static}")
-	private String region;
-	
-	@Bean
-	public AmazonS3 amazonS3Client() {
-		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-		return AmazonS3ClientBuilder
-				.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
-				.withRegion(region)
-				.build();
-	}
+//	@Value("${cloud.aws.credentials.access-key}")
+//	private String accessKey;
+//	
+//	@Value("${cloud.aws.credentials.secret-key}")
+//	private String secretKey;
+//	
+//	@Value("${cloud.aws.region.static}")
+//	private String region;
+//	
+//	@Bean
+//	public AmazonS3 amazonS3Client() {
+//		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+//		return AmazonS3ClientBuilder
+//				.standard()
+//				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+//				.withRegion(region)
+//				.build();
+//	}
 }
